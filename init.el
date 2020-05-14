88e55d26c31a1d8f981a191f659e94c706486ecf
Josh Min <joshua.min.98@gmail.com>
2020-05-13 18:12:28 -0700

    Remove custom set variables

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; Move custom set variables to different file for portability
+(setq custom-file "~/.emacs.d/custom.el")
+
+;; Use MELPA package archives
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -9,29 +13,12 @@ There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-  ;; and `package-pinned-packages`. Most users will not need or want to do this.
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   )
 (package-initialize)
 
-;; Bootstrap `use-package'
+;; Bootstrap `use-package`
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/joshmin.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (iedit general dashboard web-mode prettier-js tide magit parinfer smartparens smart-parens org-bullets which-key use-package helm evil doom-themes doom-modeline counsel-projectile all-the-icons-ivy))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
